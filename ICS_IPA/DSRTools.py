4986d07d5495a770fb47234e78f928715de0ca59
Zaid Nackasha <znackasha@intrepidcs.com>
2018-05-16 20:32:09 -0400

    removed StartDSR and LogHit function from DSRTools.py and replaced it with IncludeHit

--- ICS_IPA/DSRTools.py
+++ ICS_IPA/DSRTools.py
@@ -6,46 +6,49 @@ import os
 class DSRFile:
 	def __init__(self):
 		self.dsr =  {"HitList" : []}
+		self.data_to_hitlist = {}
 
-	def Begin(self, data, hitDiscretion = "Hit number ", initTrigger=False):
+#--------------------------------------------------------- method 1 ------------------------------------------------------
+	def Begin(self, data, hitDescription = "Hit number ", initTrigger=False):
 		self.numRec = 0
 		self.triggered = initTrigger
-		self.hitDiscretion = hitDiscretion
+		self.hitDescription = hitDescription
 		self.data = data
 		if IPAInterfaceLibrary.is_running_on_wivi_server():
 			filenamewithoutpath = os.path.basename(data.dbFile["path"])
 			self.dsr["HitList"].append({"id": data.dbFile["id"], "startDate": data.dbFile["startDate"], "vehicle": data.dbFile["vehicle"], "Filename": filenamewithoutpath})	
 		else:
 			self.dsr["HitList"].append({"FilenameAndPath": data.dbFile["path"]})
+		self.data_to_hitlist[data] = self.dsr["HitList"][-1]
 
 	def IncludeCurrentRecord(self, included):
 		if included:
 			if not self.triggered:
 				if "Hits" not in self.dsr["HitList"][-1]:
 					self.dsr["HitList"][-1]["Hits"] = []
-				self.dsr["HitList"][-1]["Hits"].append({"Description" : self.hitDiscretion + str(self.numRec), "StartTime": self.data.RecordTimestamp })
+				self.dsr["HitList"][-1]["Hits"].append({"Description" : self.hitDescription + str(self.numRec), "StartTime": self.data.RecordTimestamp })
 				self.numRec += 1
 				self.triggered = True
 		elif self.triggered:
 			self.dsr["HitList"][-1]["Hits"][-1]["EndTime"] = self.data.RecordTimestamp
-			self.triggered = False		
+			self.triggered = False
 
-	def StartDSR(self, data):
-		self.numHits = 0
-		self.triggered = False
-		self.hitDescription = "Hit number "
-		self.data = data
-		if IPAInterfaceLibrary.is_running_on_wivi_server():
-			filenamewithoutpath = os.path.basename(data.dbFile["path"])
-			self.dsr["HitList"].append({"id": data.dbFile["id"], "startDate": data.dbFile["startDate"], "vehicle": data.dbFile["vehicle"], "Filename": filenamewithoutpath})
-		else:
-			self.dsr["HitList"].append({"FilenameAndPath": data.dbFile["path"]})
+	def End(self):
+		if self.triggered:
+			self.dsr["HitList"][-1]["Hits"][-1]["EndTime"] = self.data.GetMeasurementTimeBounds()[2] - self.data.GetMeasurementTimeBounds()[1]	
+
+#--------------------------------------------------------- method 2 ------------------------------------------------------
+
+	def IncludeHit(self, data, hitStartTime, hitEndTime, hitDescription = "Include Hit"):
+		if data not in self.data_to_hitlist:
+			self.Begin(data)
+		hitlist = self.data_to_hitlist[data]
 			
-	def LogHit(self, hitDescription, hitStartTime, hitEndTime):
-		if "Hits" not in self.dsr["HitList"][-1]:
-			self.dsr["HitList"][-1]["Hits"] = []
-		self.dsr["HitList"][-1]["Hits"].append({"Description" : hitDescription, "StartTime": hitStartTime, "EndTime": hitEndTime})
-		self.numHits += 1	
+		if "Hits" not in hitlist:
+			hitlist["Hits"] = []
+		hitlist["Hits"].append({"Description" : hitDescription, "StartTime": hitStartTime, "EndTime": hitEndTime})
+
+#--------------------------------------------------------- method 3 ------------------------------------------------------
 
 	def Add(self, data, callback, hitDiscretion = "Hit number ", initTrigger=False):
 		'''
@@ -64,8 +67,3 @@ class DSRFile:
 	def save(self, filename = "data.dsr"):
 		with open(filename, 'w') as outfile:
 			json.dump(self.dsr, outfile, sort_keys=True, indent=4)
-
-	def End(self):
-		if self.triggered:
-			self.dsr["HitList"][-1]["Hits"][-1]["EndTime"] = self.data.GetMeasurementTimeBounds()[2] - self.data.GetMeasurementTimeBounds()[1]	
-
