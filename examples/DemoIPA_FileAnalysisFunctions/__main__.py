f6cd8109fc168fb3132a1a4e5f04ca577392076a
John Mitchell <jmitchell@intrepidcs.com>
2018-09-14 13:39:13 -0400

    John's changes to add white list check for eval function calls and break out some code to external class files.

---
+++ examples/DemoIPA_FileAnalysisFunctions/__main__.py
@@ -0,0 +1,1 @@
+import DemoIPA_FileAnalysisFunctions
--- examples/FindInFiles_ExpressionInConfig/FindInFilesExpInConfig.py
+++ examples/FindInFiles_ExpressionInConfig/FindInFilesExpInConfig.py
@@ -1,4 +1,36 @@
-﻿import numpy as np
+﻿##########################################################################################################################################################################################################################################
+#	Script Description:
+#		This script searches for a list of events among a list of input files. The events and the signals referenced in the events are defined in the script config file 
+# 		ConfigForSampleDataFileExpInConfig.asl. Each event has a Description, StartExpression, and EndExpression. The script generates a report file that lists the StartTime
+#		and EndTime of each event in each file. 
+#		
+###########################################################################################################################################################################################################################################
+#	Script Inputs: (when you run this script you will be prompted with 2 file open dialog windows. The first asks for a config file with extension *.asl; the second asks 
+# 	for a list of one or more data files (*.db, *.mf4, *.dat))
+#		
+# 		Script config file ConfigForSampleDataFileExpInConfig.asl is a JSON file used to configure the script. This file has the following keys:
+# 			
+# 			SignalListForUseInTimeBasis - list of the key signals that you want the script to use as time basis. When you call the GetNextRecord() function, the virtual 
+# 			time cursor will be moved to the next chronological data point among the signals in this list. If you only inlcude 1 signal that is updated at 1Hz then 
+# 			GetNextRecord() will step through the file at 1Hz matching the time stamps from this signal.
+#			
+# 			Channels - List of channels that your script will reference in the EventDefinitions
+#
+#			EventDefinitions - list of named events that script is searching for. Each event has a Description, StartExpression, and EndExpression (see sample *.asl file). 
+# 			Script looks through input file list for the StartExpression to toggle from False to True. If found, it logs the time that it occurred in the file and then 
+# 			starts looking for the EndExpression. If found, it logs the Start and End times to the dsr file. 
+#
+#		Sample Data File(s) list. Script can be run on one or more copies of the sample data file
+#			
+# 			DataSpySampleDataFileAllSignals1.db
+#
+##########################################################################################################################################################################################################################################
+#	Script Outputs:
+#		The script outputs a dsr file which stands for DataSpyReport. This file is a json file that lists all of occurrances of events found in the input data file set. 
+#		The output filename starts wtih FindInFiles_ followed by a timestamp with the time ths report was created ie: FindInFiles_09-05-18_11-24-32.dsr
+##########################################################################################################################################################################################################################################
+
+import numpy as np
 import datetime
 import pprint
 import enum
@@ -11,7 +43,7 @@ import re
 from ICS_IPA import DataFileIOLibrary as icsFI
 from ICS_IPA import DSRTools as icsDSR
 from ICS_IPA import IPAInterfaceLibrary
-from  SigEnumFile import Sig 
+from FindInFilesEventClass import FindInFilesEvents
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -27,103 +59,93 @@ log.info("Hello")
 
 slFilePath = IPAInterfaceLibrary.get_config_file()
 dbFilePaths = IPAInterfaceLibrary.get_input_file_list()
-
-#------------------------------------------------------------------------------------------------------------------
+#instantiate DSR file object
 dsr = icsDSR.DSRFile()
-#there are multiple methods to save to a dsr file.
 
-#convert expressions in syntax that can be used by eval function
 with open(slFilePath) as configFile:
 	config = json.load(configFile)
 
 ScriptChannels = config["Channels"]
 NumberOfSignals = len(ScriptChannels)
 Sig_list = [Channel['name_in_script'] for Channel in ScriptChannels]
-Events = config["EventDefinitions"]
-NumberOfExpressions = len(Events)
-SearchExpState = [False] * NumberOfExpressions
-EventActive = [False] * NumberOfExpressions
-EventActivePrev = [False] * NumberOfExpressions
-SearchExpStartTime = [0.0] * NumberOfExpressions
-SearchExpEndTime = [0.0] * NumberOfExpressions
-TimeFromExpressionStart = [0.0] * NumberOfExpressions
+EventDict = config["EventDefinitions"]
+NumberOfEvents = len(EventDict)
 
-EventDescription_list = [Event['Description'] for Event in Events]
-StartExpression_list = [Event['StartExpression'] for Event in Events]
+#instantiate FindInFilesEvents object
+Events = FindInFilesEvents(config)
 
-StartExpressionEval = []
-for expression in StartExpression_list:
-	ConvertedExpression = expression
-	for signal in Sig_list:
-		ConvertedExpression = re.sub(r'\b' + signal + r"\b", 'dataPoints[Sig.'+ signal + ']', ConvertedExpression)
-	#now check for Prev__ indicating a desire to reference previous loop values
-	PrevRecList = re.findall(r'\b' +'Prev__'+r'\w+', ConvertedExpression)
-	for PrevRecSig in PrevRecList:
-		ConvertedExpression = re.sub(PrevRecSig, 'dataPointsPrev[Sig.'+ PrevRecSig[6-len(PrevRecSig):] + ']', ConvertedExpression, count=1)
-	StartExpressionEval.append(ConvertedExpression)
+#if any of the event expressions failed the white list check, log which token failed for each expression then exit
+BlackTokenFound = False
+for i in range(Events.NumberOfEvents):
+	if not(Events.StartExpressionTokensAreInWhiteList[i]):
+		log.info("The start expression in the event called " + Events.EventDescriptions[i] + " failed the whitelist check due to the token " + Events.FirstBlackTokenInStartExpression[i])
+		BlackTokenFound = True
 
+	if not(Events.EndExpressionTokensAreInWhiteList[i]):
+		log.info("The end expression in the event called " + Events.EventDescriptions[i] + " failed the whitelist check due to the token " + Events.FirstBlackTokenInEndExpression[i])
+		BlackTokenFound = True
+if BlackTokenFound:
+	exit()
 
-EndExpression_list = [Event['EndExpression'] for Event in Events]
-EndExpressionEval = []
-for expression in EndExpression_list:
-	ConvertedExpression = expression
-	for signal in Sig_list:
-		ConvertedExpression = re.sub(signal, 'dataPoints[Sig.'+ signal + ']', ConvertedExpression)
-	#now check for Prev__ indicating a desire to reference previous loop values
-	PrevRecList = re.findall(r'\b' +'Prev__'+r'\w+', ConvertedExpression)
-	for PrevRecSig in PrevRecList:
-		ConvertedExpression = re.sub(PrevRecSig, 'dataPointsPrev[Sig.'+ PrevRecSig[6-len(PrevRecSig):] + ']', ConvertedExpression, count=1)
-	ConvertedExpression = re.sub('TimeFromExpStart', 'TimeFromExpressionStart[i]', ConvertedExpression)
-	EndExpressionEval.append(ConvertedExpression)
-
-#now create a string for SetActiveMask with only first signal active
+#now create a string for SetActiveMask 
 SetActiveMaskString = ''
 NumberOrSignals = 0
 for signal in Sig_list:
-	if signal == "AccelPedalPosition":
+	NumberOfTimeBasisSignals = len(config["SignalListForUseInTimeBasis"])
+	CurrentSignalInTimeBasisList = False
+	for i in range(NumberOfTimeBasisSignals):
+		if signal == config["SignalListForUseInTimeBasis"][i]:
+			CurrentSignalInTimeBasisList = True
+	if CurrentSignalInTimeBasisList:
 		SetActiveMaskString = SetActiveMaskString + '1'
 	else:
 		SetActiveMaskString = SetActiveMaskString + '0'
 	NumberOrSignals = NumberOrSignals + 1
 
+#now go through each file, look for hits and log hits to dsr file
+FileNumber = 0
 for dbFilePath in dbFilePaths:
 	try:
+		FileNumber = FileNumber + 1
 		with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:
+			ActiveMaskResult = data.SetActiveMask(SetActiveMaskString)
 			curTimestamp = data.JumpBeforeTimestamp(0)
 			dataPoints = data.GetPoints()
 			dataPointsPrev = dataPoints.copy()
-			ActiveMaskResult = data.SetActiveMask(SetActiveMaskString)
+
 			#initialize event based arrays
-			for i, expressionStart in enumerate(StartExpressionEval):
-				EventActive[i] = False
-				EventActivePrev[i] = False
-				SearchExpState[i] = False			
+			RecordIncludesExpressionEndEvent = False
 			while curTimestamp != sys.float_info.max:
-				for i, expressionStart in enumerate(StartExpressionEval):
-					if EventActive[i] == False:
-						SearchExpState[i] = eval(expressionStart)	
+				CurrentRecordHasBeenAdded = False
+				for i in range(Events.NumberOfEvents):
+					if Events.EventActive[i] == False:
+						Events.SearchExpState[i] = eval(Events.StartExpressionFormattedForEval[i])
+						RecordIncludesExpressionEndEvent = False	
 					else:
-						SearchExpState[i] = eval(EndExpressionEval[i])
-					if EventActive[i] == False and SearchExpState[i] == True:
-						SearchExpStartTime[i] = curTimestamp
-						EventActive[i] = True
-						TimeFromExpressionStart[i] = 0
-					if EventActive[i] and EventActivePrev[i]:
-						if SearchExpState[i] == True:
-							SearchExpEndTime[i] = curTimestamp
-							dsr.IncludeHit(data, SearchExpStartTime[i], SearchExpEndTime[i], EventDescription_list[i])
-							EventActive[i] = False
-							TimeFromExpressionStart[i] = - 1
+						Events.SearchExpState[i] = eval(Events.EndExpressionFormattedForEval[i])
+						if Events.SearchExpState[i]:
+							RecordIncludesExpressionEndEvent = True
+					if Events.EventActive[i] == False and Events.SearchExpState[i] == True:
+						Events.SearchExpStartTime[i] = curTimestamp
+						Events.EventActive[i] = True
+						Events.TimeFromExpressionStart[i] = 0
+					if Events.EventActive[i] and Events.EventActivePrev[i]: #this is TRUE first loop with EndExpressionEval[i] = TRUE
+						if Events.SearchExpState[i] == True:
+							Events.SearchExpEndTime[i] = curTimestamp
+							dsr.IncludeHit(data, Events.SearchExpStartTime[i], Events.SearchExpEndTime[i], Events.EventDescriptions[i])
+							Events.EventActive[i] = False
+							Events.TimeFromExpressionStart[i] = - 1
 						else:
-							TimeFromExpressionStart[i] = curTimestamp - SearchExpStartTime[i]
-					EventActivePrev[i] = EventActive[i]
+							Events.TimeFromExpressionStart[i] = curTimestamp - Events.SearchExpStartTime[i]
+					Events.EventActivePrev[i] = Events.EventActive[i]
 				dataPointsPrev = dataPoints.copy()# copy previous loops record array to new array
 				curTimestamp = data.GetNextRecord()
+				#log.info(str(curTimestamp))
 			#if any events are active at the end of file, log the last time stamp as the end of hit
-			for i, expressionStart in enumerate(StartExpressionEval):
-				if EventActive[i]:
-					dsr.IncludeHit(data, SearchExpStartTime[i], data.GetMeasurementTimeBounds()[2] - data.GetMeasurementTimeBounds()[1], EventDescription_list[i])
-
+			for i in range(Events.NumberOfEvents):
+				if Events.EventActive[i]:
+					dsr.IncludeHit(data, Events.SearchExpStartTime[i], data.GetMeasurementTimeBounds()[2] - data.GetMeasurementTimeBounds()[1], Events.EventDescriptions[i])
+	
 	except ValueError as e :
 		print(str(e))
  
