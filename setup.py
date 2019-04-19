7fd1acb64211d0b7e8a050f44040e67bdacd5212
Zaid Nackasha <znackasha@intrepidcs.com>
2019-04-19 00:20:42 -0400

    added dynamic python support.

--- setup.py
+++ setup.py
@@ -1,22 +1,47 @@
 from setuptools import setup
+import os
+import platform
 
-version = '0.0.4.6'
+version = '0.4.9'
+
+print("seting up for " + platform.system() + " " + platform.architecture()[0] + " platform")
+if platform.system() == 'Windows' and platform.architecture()[0] == '32bit':
+    os.rename("./ICS_IPA/_DataFileIOLibraryInterface32.pyd", "./ICS_IPA/_DataFileIOLibraryInterface.pyd")
+elif platform.system() == 'Windows' and platform.architecture()[0] == '64bit':
+    os.rename("./ICS_IPA/_DataFileIOLibraryInterface64.pyd", "./ICS_IPA/_DataFileIOLibraryInterface.pyd")
+elif platform.system() == 'Linux':
+    os.rename("./ICS_IPA/_DataFileIOLibraryInterface64.so", "./ICS_IPA/_DataFileIOLibraryInterface.so")
+else:
+    raise "Platform is not supported"
 
 setup(
-	name = 'ICS_IPA',
-	packages = ['ICS_IPA'],
-	version = version,
-	description = 'API used for DataMining mdf data files using DataSpy',
-	long_description='This repo is designed to manage the library functions used for DataMining through mdf data files using the DataSpy product made by Intrepid Control System. The library contains a bunch of File I/O functions in a dll that allow users to parse through mdf data files using their own applications like Python, Excel, Matlab, C# etc. This library of functions is duplicated on Intrepids wireless data server (Wireless NeoVI) allowing users to develop scripts on their PC and then run those scripts on the Wireless Neo VI data server without requiring the data to be downloaded.',
-	maintainer = 'Zaid Nackasha',
-	maintainer_email = 'ZNackasha@intrepidcs.com',
-	url = 'https://github.com/intrepidcs/ICS_IPA',
-	download_url = 'https://github.com/intrepidcs/ICS_IPA/archive/'+ version + '.tar.gz',
-	package=['DataFileIOLibrary'],
-	package_data = {'DataFileIOLibrary': ['ICS_IPA/_DataFileIOLibraryInterface.pyd', 'ICS_IPA/_DataFileIOLibraryInterface.so']},
-	include_package_data=True,
-	classifiers = ['Operating System :: Microsoft :: Windows',
-				'Programming Language :: Python',
-				'Programming Language :: Python :: 3.6',
-  ]
+    name='ICS_IPA',
+    packages=['ICS_IPA'],
+    version=version,
+    description='API used for DataMining mdf data files using DataSpy',
+    long_description='This repo is designed to manage the library functions used \
+                      for DataMining through mdf data files using the DataSpy \
+                      product made by Intrepid Control System. The library \
+                      contains a bunch of File I/O functions in a dll that \
+                      allow users to parse through mdf data files using their \
+                      own applications like Python, Excel, Matlab, C# etc. \
+                      This library of functions is duplicated on Intrepids \
+                      wireless data server (Wireless NeoVI) allowing users to \
+                      develop scripts on their PC and then run those scripts \
+                      on the Wireless Neo VI data server without requiring \
+                      the data to be downloaded.',
+    maintainer='Zaid Nackasha',
+    maintainer_email='ZNackasha@intrepidcs.com',
+    url='https://github.com/intrepidcs/ICS_IPA',
+    download_url='https://github.com/intrepidcs/ICS_IPA/archive/' +
+                 version + '.tar.gz',
+    package=['DataFileIOLibrary'],
+    package_data={'DataFileIOLibrary':
+                  ['ICS_IPA/_DataFileIOLibraryInterface32.pyd',
+                   'ICS_IPA/_DataFileIOLibraryInterface64.pyd',
+                   'ICS_IPA/_DataFileIOLibraryInterface64.so']},
+    include_package_data=True,
+    classifiers=['Operating System :: Microsoft :: Windows',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 3.6']
 )
--- test/read_and_generate_dsr.py
+++ test/read_and_generate_dsr.py
@@ -10,8 +10,9 @@ from ICS_IPA import DataFileIOLibrary as icsFI
 from ICS_IPA import DSRTools as icsDSR
 from ICS_IPA import IPAInterfaceLibrary
 
-slFilePath = "../examples/findInFiles/ConfigForSampleDataFileDemo.asl"
-dbFilePaths = [{"path": "../examples/Data/SampleDataFiles/DataSpySampleDataFileAllSignals1.db"}]
+slFilePath = "./examples/findInFiles/ConfigForSampleDataFileDemo.asl"
+dbFilePaths = [
+    {"path": "./examples/Data/SampleDataFiles/DataSpySampleDataFileAllSignals1.db"}]
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -28,133 +29,146 @@ log.info("Hello")
 dsr = icsDSR.DSRFile()
 # there are multiple methods to save to a dsr file.
 
-# the loop method 
+# the loop method
 
 for dbFilePath in dbFilePaths:
-	try :
-		with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:
+    try:
+        with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:
 
-			accelpedalPostionIndex = data.indexOfSignal("AccelPedalPosition")
-			transOutputSpeedIndex = data.indexOfSignal("TransOutputSpeed")
-			
-			curTimestamp = data.JumpAfterTimestamp(0)
-			dataPoints = data.GetPoints()
-			dsr.Begin(data)
-			while curTimestamp != sys.float_info.max:
-				dsr.IncludeCurrentRecord(dataPoints[accelpedalPostionIndex] > 80 and dataPoints[transOutputSpeedIndex] < 1600)
-				curTimestamp = data.GetNextRecord()
-			dsr.End()
-	except ValueError as e :
-		print(str(e))
- 
+            accelpedalPostionIndex = data.indexOfSignal("AccelPedalPosition")
+            transOutputSpeedIndex = data.indexOfSignal("TransOutputSpeed")
+
+            curTimestamp = data.JumpAfterTimestamp(0)
+            dataPoints = data.GetPoints()
+            dsr.Begin(data)
+            while curTimestamp != sys.float_info.max:
+                dsr.IncludeCurrentRecord(
+                    dataPoints[accelpedalPostionIndex] > 80 and dataPoints[transOutputSpeedIndex] < 1600)
+                curTimestamp = data.GetNextRecord()
+            dsr.End()
+    except ValueError as e:
+        print(str(e))
+
 # ------------------------------------------------------------------------------------------------------------------
 
-# the i'll do everything method 
+# the i'll do everything method
 
 for dbFilePath in dbFilePaths:
-	try :
-		trigger = False
-		start = 0
-		with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:
+    try:
+        trigger = False
+        start = 0
+        with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:
 
-			accelpedalPostionIndex = data.indexOfSignal("AccelPedalPosition")
-			transOutputSpeedIndex = data.indexOfSignal("TransOutputSpeed")
+            accelpedalPostionIndex = data.indexOfSignal("AccelPedalPosition")
+            transOutputSpeedIndex = data.indexOfSignal("TransOutputSpeed")
 
-				
-			curTimestamp = data.JumpAfterTimestamp(0)
-			dataPoints = data.GetPoints()
-			while curTimestamp != sys.float_info.max:
-				query = dataPoints[accelpedalPostionIndex] > 80 and dataPoints[transOutputSpeedIndex] < 1600
-				if query and not trigger:
-					trigger = not trigger
-					start = curTimestamp
-				elif not query and trigger:
-					trigger = not trigger
-					dsr.IncludeHit(data, start, curTimestamp, "hit test")
-				curTimestamp = data.GetNextRecord()
-	except ValueError as e :
-		print(str(e))
+            curTimestamp = data.JumpAfterTimestamp(0)
+            dataPoints = data.GetPoints()
+            while curTimestamp != sys.float_info.max:
+                query = dataPoints[accelpedalPostionIndex] > 80 and dataPoints[transOutputSpeedIndex] < 1600
+                if query and not trigger:
+                    trigger = not trigger
+                    start = curTimestamp
+                elif not query and trigger:
+                    trigger = not trigger
+                    dsr.IncludeHit(data, start, curTimestamp, "hit test")
+                curTimestamp = data.GetNextRecord()
+    except ValueError as e:
+        print(str(e))
 
-#------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------
 
-# the lamda method 
+# the lamda method
 for dbFilePath in dbFilePaths:
-	try :
-		with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:
+    try:
+        with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:
 
-			accelpedalPostionIndex = data.indexOfSignal("AccelPedalPosition")
-			transOutputSpeedIndex = data.indexOfSignal("TransOutputSpeed")
-			# using lambdas, you can think of lambdas as functions without a name 
-			# the following lambda has two parameters v and t. v for values and t for time
-			# the AddToDSR function takes a function with two paramaters as an argument
-			# The AddToDSR calls the function for every data point it iterates though to determan if it should be included in the DSR file
-			# The following two calls do the same thing in two diffrent ways the first uses a lambda
+            accelpedalPostionIndex = data.indexOfSignal("AccelPedalPosition")
+            transOutputSpeedIndex = data.indexOfSignal("TransOutputSpeed")
+            # using lambdas, you can think of lambdas as functions without a name
+            # the following lambda has two parameters v and t. v for values and t for time
+            # the AddToDSR function takes a function with two paramaters as an argument
+            # The AddToDSR calls the function for every data point it iterates though to determan if it should be included in the DSR file
+            # The following two calls do the same thing in two diffrent ways the first uses a lambda
 
-			dsr.Add(data, lambda v, t: v[accelpedalPostionIndex] > 80 and v[transOutputSpeedIndex] < 1600)
-	except ValueError as e :
-		print(str(e))
-#------------------------------------------------------------------------------------------------------------------
+            dsr.Add(data, lambda v, t: v[accelpedalPostionIndex]
+                    > 80 and v[transOutputSpeedIndex] < 1600)
+    except ValueError as e:
+        print(str(e))
+# ------------------------------------------------------------------------------------------------------------------
 
-# simply passes a function
+
 class Sig:
-	TransOutputSpeed = 0
-	AccelPedalPosition = -1 
+    ''' simply passes a function '''
+    TransOutputSpeed = 0
+    AccelPedalPosition = -1
 
-def checkFullThrotel (values, timestamp):
-	return values[Sig.AccelPedalPosition] > 80 and values[Sig.TransOutputSpeed] < 1600
+
+def checkFullThrotel(values, timestamp):
+    return values[Sig.AccelPedalPosition] > 80 and values[Sig.TransOutputSpeed] < 1600
 
 
 for dbFilePath in dbFilePaths:
-	try :
-		with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:	
-			dsr.Add(data, checkFullThrotel, "Full Throtel hit")
+    try:
+        with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:
+            dsr.Add(data, checkFullThrotel, "Full Throtel hit")
 
-	except ValueError as e :
-		print(str(e))
+    except ValueError as e:
+        print(str(e))
 # ------------------------------------------------------------------------------------------------------------------
-# using a class
+
+
 class dataCheck:
-	def __init__(self, data):
-		self.AccelPedalPositionIndex = data.indexOfSignal("AccelPedalPosition")
-		self.TransOutputSpeedIndex = data.indexOfSignal("TransOutputSpeed")
-		self.AccelPedalPositionMax = 80
-		self.TransOutputSpeedMin = 1600
+    ''' using a class '''
 
-	def __call__(self, values, timestamp): # this is the required callback function for the class
-		return values[self.AccelPedalPositionIndex] > self.AccelPedalPositionMax and values[self.TransOutputSpeedIndex] < self.TransOutputSpeedMin
+    def __init__(self, data):
+        self.AccelPedalPositionIndex = data.indexOfSignal("AccelPedalPosition")
+        self.TransOutputSpeedIndex = data.indexOfSignal("TransOutputSpeed")
+        self.AccelPedalPositionMax = 80
+        self.TransOutputSpeedMin = 1600
+
+    # this is the required callback function for the class
+    def __call__(self, values, timestamp):
+        return values[self.AccelPedalPositionIndex] > self.AccelPedalPositionMax and values[self.TransOutputSpeedIndex] < self.TransOutputSpeedMin
 
 
 for dbFilePath in dbFilePaths:
-	try :
-		with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:		
-			dc = dataCheck(data)
-			dsr.Add(data, dc, hitDiscretion = "Class Example hit")
-	except ValueError as e :
-		print(str(e))
+    try:
+        with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:
+            dc = dataCheck(data)
+            dsr.Add(data, dc, hitDiscretion="Class Example hit")
+    except ValueError as e:
+        print(str(e))
 
 # ------------------------------------------------------------------------------------------------------------------
 
-# uses a class inwich the starting point of the file is used as max trigger
-class AdvancedDataCheck:
-	def __init__(self, data):
-		self.AccelPedalPositionIndex = data.indexOfSignal("AccelPedalPosition")
-		self.TransOutputSpeedIndex = data.indexOfSignal("TransOutputSpeed")
-		self.previousAccelPedalPosition = None
-		self.TransOutputSpeedMin = 1600
 
-	def __call__(self, values, timestamp): # this is the required callback function for the class
-		if self.previousAccelPedalPosition is None:
-			self.previousAccelPedalPosition = values[self.AccelPedalPositionIndex]
-			return true
-		return values[self.AccelPedalPositionIndex] > self.AccelPedalPositionMax and values[self.TransOutputSpeedIndex] < self.TransOutputSpeedMin
+class AdvancedDataCheck:
+
+
+''' uses a class witch the starting point of the file is used as max trigger '''
+
+    def __init__(self, data):
+        self.AccelPedalPositionIndex = data.indexOfSignal("AccelPedalPosition")
+        self.TransOutputSpeedIndex = data.indexOfSignal("TransOutputSpeed")
+        self.previousAccelPedalPosition = None
+        self.AccelPedalPositionMax = 80
+        self.TransOutputSpeedMin = 1600
+
+    # this is the required callback function for the class
+    def __call__(self, values, timestamp):
+        if self.previousAccelPedalPosition is None:
+            self.previousAccelPedalPosition = values[self.AccelPedalPositionIndex]
+            return True
+        return values[self.AccelPedalPositionIndex] > self.AccelPedalPositionMax and values[self.TransOutputSpeedIndex] < self.TransOutputSpeedMin
 
 for dbFilePath in dbFilePaths:
-	try :
-		with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:
-			dc = AdvancedDataCheck(data)
-			dsr.Add(data, dc, hitDiscretion = "Class Example hit")
-	except ValueError as e :
-		print(str(e))
+    try:
+        with icsFI.ICSDataFile(dbFilePath, slFilePath) as data:
+            dc = AdvancedDataCheck(data)
+            dsr.Add(data, dc, hitDiscretion="Class Example hit")
+    except ValueError as e:
+        print(str(e))
 
 # ------------------------------------------------------------------------------------------------------------------
 dsr.save("test.dsr")
